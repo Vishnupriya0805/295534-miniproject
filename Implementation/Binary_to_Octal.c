180cc68fe31fd54afc0be17cf48df0a882da61a0
Vishnupriya-295534 <80520183+Vishnupriya0805@users.noreply.github.com>
2021-04-13 18:55:18 +0530

    Add files via upload

---
+++ Implementation/Binary_to_Octal.c
@@ -0,0 +1,19 @@
+#include"cal.h"
+int Binary_to_Octal(long binarynum)
+{
+    int octalnum = 0, decimalnum = 0, i = 0;
+    while(binarynum != 0)
+    {
+        decimalnum = decimalnum + (binarynum%10) * pow(2,i);
+        i++;
+        binarynum = binarynum / 10;
+    }
+    i = 1;
+    while (decimalnum != 0)
+    {
+        octalnum = octalnum + (decimalnum % 8) * i;
+        decimalnum = decimalnum / 8;
+        i = i * 10;
+    }
+    return octalnum;
+}
---
+++ Implementation/cal.h
@@ -0,0 +1,16 @@
+#ifndef __CAL_H__ 
+#define __CAL_H__
+
+long Decimal_to_Binary(int decimalnum);
+
+int Binary_to_Decimal(long binarynum);
+
+int Binary_to_Octal(long binarynum);
+
+long Octal_to_Binary(int octalnum);
+
+int Decimal_to_Octal(int decimalnum);
+
+long Octal_to_Decimal(int octalnum);
+
+#endif 
